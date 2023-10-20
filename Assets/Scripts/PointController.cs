d058c5c09223c94ea4903718b220c679e3e5df16
IvanLindgren <denis.kornilov12@yandex.ru>
2023-10-20 22:53:32 +0300

    полки

--- Assets/Scripts/PointController.cs
+++ Assets/Scripts/PointController.cs
@@ -1,15 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.AssemblyQualifiedNameParser;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PointController : MonoBehaviour
 {
-
+    private UiController ui;
     
     void Start()
     {
         Cursor.visible = true;
+        ui = FindObjectOfType<UiController>();
     }
 
   
@@ -24,10 +27,26 @@ public class PointController : MonoBehaviour
                     CheckCollider().GetComponent<VnukData>().ChangeState(1);
                 } else if (CheckCollider().name == "WardrobeTrigger")
                 {
-                    FindObjectOfType<UiController>().ChangeBackState(1);
+                    if (ui.nowState == 0)
+                    {
+                        ui.ChangeBackState(1);
+                    } else
+                    {
+                        ui.ActiveBook();
+                    }
+    
+                }
+                if (CheckCollider().tag == "book")
+                {
+                    var firstBook = CheckCollider();
+                    var secondBook = GameObject.Find((Int32.Parse((firstBook.name)) + 1).ToString());
+                    if (secondBook != null)
+                    {
+                        ui.BookChanger(firstBook, secondBook);
+                    }
                 }
             }
-        }
+        } 
     }
 
     private GameObject CheckCollider()
